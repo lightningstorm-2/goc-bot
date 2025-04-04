34042018bec8a69ea3445ed4b042456ead152b32
marcelo761 <marcelo.freitas44@etec.sp.gov.br>
2025-04-04 11:08:09 -0300

    Update balance.js
    The integer values should work now

--- src/schemas/balance.js
+++ src/schemas/balance.js
@@ -3,7 +3,7 @@ const balanceSchema = new Schema({
   _id: Schema.Types.ObjectId,
   userId: String,
   guildId: String,
-  balance: { type: Number, default: 0 },
+  balance: { type: Number, default: 0, validate: Number.isInteger },
 });
 
 module.exports = model("Balance", balanceSchema, "balances");
